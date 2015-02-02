05c1ba002a610897ffcadbeccd2b841831078f61
Ilya Arkhanhelsky <dm@dm>
2015-02-02 15:15:08 +0300

    draw_line

--- src/main.py
+++ src/main.py
@@ -27,6 +27,32 @@ def write_image(image, file):
     '''Writes image to a given file'''
     image.save(file)
 
+def draw_line(image, begin, end, color):
+    '''Draw straight line begin point to end point'''
+    '''with given color'''
+    steep = False # if line is steep, we transpose the image
+    if abs(begin.x - end.x) < abs(begin.y - end.y) :
+        begin = Point(x = begin.y, y = begin.x)
+        end = Point(x = end.y, y = end.x)
+        steep = True
+
+    if begin.x > end.x :
+        (x, y) = begin
+        begin = Point(x = end.x, y = end.y)
+        end = Point(x = x, y= y)
+
+    for x in range(begin.x, end.x):
+        t = (x - begin.x)/ (end.x - begin.x)
+        y = (int) (begin.y * (1 - t) + end.y * t)
+        set_pixel(image, (y, x) if steep else (x, y), color)
+
+
 img = create_image((100, 100))
 set_pixel(img, Point(x=52, y=41), red)
+draw_line(img, Point(13, 20), Point(80, 40), white)
+draw_line(img, Point(20, 13), Point(40, 80), red)
+draw_line(img, Point(80, 40), Point(13, 20), red)
 write_image(img, "out.tga")
+
+
+

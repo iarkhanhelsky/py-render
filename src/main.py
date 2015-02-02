91976abb9f4ab212c4638d5eea03b9483b8ab462
Ilya Arkhanhelsky <dm@dm>
2015-02-02 15:26:07 +0300

    empty lines @ main.py

--- src/main.py
+++ src/main.py
@@ -4,9 +4,6 @@ from utils import Point
 from utils import White
 from utils import Red
 
-
-
-
 img = create_image((100, 100))
 set_pixel(img, Point(x=52, y=41), Red)
 draw_line(img, Point(13, 20), Point(80, 40), White)
